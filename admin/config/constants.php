b929de46b216624c5e9d29d1ce7d555abcb799fd
kusemererwa <kusemererwaedson2000@gmail.com>
2025-04-29 17:43:36 +0300

    config

--- admin/config/constants.php
+++ admin/config/constants.php
@@ -16,7 +16,3 @@ define('DB_PASS', 'Eddiespacks2000@');
 define('DB_NAME', 'hakabvzf_edblog');
 
 
-//// ALTERING TABLES SQL
-// ALTER TABLE posts ADD CONSTRAINT FK_blog_category FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE SET NULL;
-
-// ALTER TABLE posts ADD CONSTRAINT FK_blog_author FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE CASCADE;
--- config/constants.php
+++ config/constants.php
@@ -1,14 +1,12 @@
 <?php
-// //when online
+// // when offline
 // session_start();
-// define('ROOT_URL', 'https://ibelieve.hakateq.com/');
+// define('ROOT_URL', 'http://localhost/ibelieve/');
 // define('DB_HOST', 'localhost');
-// define('DB_USER', 'hakacdmf_edson');
-// define('DB_PASS', 'edson2000');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
 // define('DB_NAME', 'hakacdmf_edblog');
 
-
-// when offline 
 // when online
 session_start();
 define('ROOT_URL', 'http://ibelieve.hakateq.com/');
@@ -16,4 +14,5 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'hakabvzf_edson');
 define('DB_PASS', 'Eddiespacks2000@');
 define('DB_NAME', 'hakabvzf_edblog');
+
 
